dae2712217f5d5c2a1d1a4a64c8c3c4800af0647
hobbyscripterII <wruomma2@naver.com>
2025-04-12 12:05:23 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/project/booklog/MyBooklogApplication.java
@@ -0,0 +1,13 @@
+package com.project.booklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBooklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBooklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/booklog/MyBooklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBooklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
